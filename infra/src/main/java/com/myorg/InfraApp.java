e7df00dc0a1bb57579a62b7f93db1e26ce9fed0d
Abhinav-vats <50848068+Abhinav-vats@users.noreply.github.com>
2025-10-10 00:50:19 +0530

    account no added

--- infra/src/main/java/com/myorg/InfraApp.java
+++ infra/src/main/java/com/myorg/InfraApp.java
@@ -10,9 +10,20 @@ public class InfraApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new InfraStack(app, "LambdaCDKStack", StackProps.builder()
+        String deployEnv = System.getenv("DEPLOY_ENV");
+        String region = System.getenv("AWS_DEFAULT_REGION");
 
-                .build());
+
+        if ("test".equalsIgnoreCase(deployEnv)) {
+            new InfraStack(app, "dummy", StackProps.builder()
+                    .env(Environment.builder()
+                            .account("000000000000")
+                            .region(region)
+                            .build())
+                    .build());
+        }else{
+            new InfraStack(app, "LambdaCDKStack", StackProps.builder().build());
+        }
 
         app.synth();
     }

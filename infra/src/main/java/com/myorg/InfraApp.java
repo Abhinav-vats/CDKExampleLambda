a11a9f5f7b6b0394b626f00a85fc976c0d51e059
Abhinav-vats <50848068+Abhinav-vats@users.noreply.github.com>
2025-10-10 02:10:53 +0530

    final deployment code

--- infra/src/main/java/com/myorg/InfraApp.java
+++ infra/src/main/java/com/myorg/InfraApp.java
@@ -14,29 +14,9 @@ public class InfraApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        String deployEnv = System.getenv("DEPLOY_ENV");
-        String region = System.getenv("CDK_DEPLOY_REGION");
 
+        new InfraStack(app, "LambdaCDKStack", StackProps.builder().build());
 
-        if ("test".equalsIgnoreCase(deployEnv)) {
-            new InfraStack(app, "dummy", StackProps.builder()
-                    .env(Environment.builder()
-                            .account("000000000000")
-                            .region(region)
-                            .build())
-                    .build());
-
-            SnsClient snsClient = SnsClient.builder()
-                    .endpointOverride(URI.create(System.getenv("LOCALSTACK_ENDPOINT")))
-                    .region(Region.of(System.getenv("CDK_DEPLOY_REGION")))
-                    .credentialsProvider(StaticCredentialsProvider.create(
-                            AwsBasicCredentials.create("test","test")
-                    ))
-                    .build();
-
-        }else{
-            new InfraStack(app, "LambdaCDKStack", StackProps.builder().build());
-        }
 
         app.synth();
     }
